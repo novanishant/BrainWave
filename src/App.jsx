7482b300bd6d429cc3445000779934443162b097
novanishant <nishantverma3633@gmail.com>
2024-03-05 15:42:35 +0530

    logo for brainWave

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-
-function App() {
-  return <div className="text-3xl font-bold underline">hello world</div>;
-}
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">hello world</h1>;
+    </>
+  );
+};
 
 export default App;
+ 
